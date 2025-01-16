b0d6a71f778e78c03500f64ff131ec55af949b3e
soniahammou <sonia.hammou@oclock.school>
2025-01-16 20:40:12 +0100

    feat/ajout de la page 404 avec feuille de style

---
+++ wp-content/themes/montheme/404.php
@@ -0,0 +1,16 @@
+<?php get_header() ?>
+
+//TODO mettre le lien
+<section class="error404_section ">
+
+  <div class="error404_section-content">
+
+    <h1>La recette est introuvable...</h1>
+    <p>Mais pas de panique, nos meilleurs chefs travaillent dessus ! </p>
+    <p> En attendant, découvrez <a href="#">notre menu</a> pour vous régaler.</p>
+
+  </div>
+
+</section>
+
+<?php get_footer() ?>
